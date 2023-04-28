ed78dede9d2844ed1cb5429b7e0bb419735d6b60
PsicloneCB <cristian.buca@playtech.com>
2023-04-28 12:55:42 +0100

    Refactor->index.js: player logic into own class

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import * as PIXI from "pixi.js";
 import Victor from "victor";
+import Player from "./player";
 //import Matter from "matter-js";
 
 const canvasSize = 256;
@@ -13,14 +14,7 @@ const app = new PIXI.Application({
 
 globalThis.__PIXI_APP__ = app;
 
-const  squareWidth = 32;
-const square = new PIXI.Sprite(PIXI.Texture.WHITE);
-square.anchor.set(0.5);
-square.position.set(app.screen.width / 2, app.screen.height / 2);
-square.width = square.height = squareWidth;
-square.tint = 0xea985d;
-
-app.stage.addChild(square);
+let player = new Player({ app });
 
 const enemyRadius = 16;
 const enemySpeed = 2;
@@ -34,19 +28,19 @@ app.stage.addChild(enemy);
 
 app.ticker.add((delta) =>
 {
-	const cursorPosition = app.renderer.plugins.interaction.mouse.global;
-	let angle = Math.atan2
-	(
-		cursorPosition.y- square.position.y,
-		cursorPosition.x - square.position.x
-	) + Math.PI / 2;
-	square.rotation = angle;
+	player.update();
+	// let e = new Victor(enemy.position.x, enemy.position.y);
+	// let s = new Victor(square.position.x, square.position.y);
 
-	let e = new Victor(enemy.position.x, enemy.position.y);
-	let s = new Victor(square.position.x, square.position.y);
-	let d = s.subtract(e);
-	let v = d.normalize().multiplyScalar(enemySpeed);
-	enemy.position.set(enemy.position.x + v.x, enemy.position.y + v.y);
+	// if (e.distance(s) < squareWidth / 2)
+	// {
+	// 	let r = randomSpawnPoint();
+	// 	enemy.position.set(r.x, r.y);
+	// 	return
+	// }
+	// let d = s.subtract(e);
+	// let v = d.normalize().multiplyScalar(enemySpeed);
+	// enemy.position.set(enemy.position.x + v.x, enemy.position.y + v.y);
 });
 
 function randomSpawnPoint()
